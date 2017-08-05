d0845187c5e53d7298cb11efaa8aa58c6c541b12
Nathan Reline <nathanielreline@gmail.com>
2017-08-05 15:19:25 -0700

    Move store configuration to its own file

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,21 @@
-import { createHashHistory  } from 'history';
+import {createHashHistory} from 'history';
 import React from "react";
 import ReactDom from "react-dom";
-import { Provider } from 'react-redux'
-import { ConnectedRouter, routerMiddleware} from 'react-router-redux';
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-// import createLogger from 'redux-logger';
-import thunk from 'redux-thunk';
-
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux';
 import App from "./container/App";
-import RootReducer from './reducer/RootReducer'
+
+import configureStore from './store/configureStore'
 
 // Hash history is needed to start the URL at "/" instead of "file:///"
 const history = createHashHistory();
 
-let middleware = applyMiddleware(thunk, routerMiddleware(history));
-
-// TODO: Remove for production builds
-middleware = composeWithDevTools(middleware);
-
-const store = createStore(RootReducer, middleware);
+const store = configureStore(history);
 
 ReactDom.render((
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <App />
+            <App/>
         </ConnectedRouter>
     </Provider>
 ), document.getElementById('root'));
---
+++ src/store/configureStore.js
@@ -0,0 +1,17 @@
+import PouchDB from 'pouchdb'
+import {routerMiddleware} from 'react-router-redux';
+import {applyMiddleware, createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import RootReducer from '../reducer/RootReducer'
+import thunk from 'redux-thunk';
+
+export default function configureStore(history) {
+    const db = new PouchDB('united-client-db');
+
+    let middleware = applyMiddleware(thunk, routerMiddleware(history));
+
+    // TODO: Remove for production builds
+    middleware = composeWithDevTools(middleware);
+
+    return createStore(RootReducer, middleware)
+}
